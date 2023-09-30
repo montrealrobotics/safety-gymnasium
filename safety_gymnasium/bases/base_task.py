b814622e8d1ba40083b2aa1feae5c8acfe28ef8a
kaustubh <kaustubh3095@gmail.com>
2023-09-30 17:26:49 -0400

    working

--- safety_gymnasium/bases/base_task.py
+++ safety_gymnasium/bases/base_task.py
@@ -44,7 +44,7 @@ class LidarConf:
         alias (bool): Lidar bins alias into each other.
     """
 
-    num_bins: int = 16
+    num_bins: int = 32
     max_dist: float = 3
     exp_gain: float = 1.0
     type: str = 'pseudo'
@@ -231,14 +231,14 @@ class BaseTask(Underlying):  # pylint: disable=too-many-instance-attributes,too-
         obs_space_dict.update(self.agent.build_sensor_observation_space())
 
         for obstacle in self._obstacles:
-            if obstacle.is_lidar_observed:
-                name = obstacle.name + '_' + 'lidar'
-                obs_space_dict[name] = gymnasium.spaces.Box(
-                    0.0,
-                    1.0,
-                    (self.lidar_conf.num_bins,),
-                    dtype=np.float64,
-                )
+            # if obstacle.is_lidar_observed:
+                # name = obstacle.name + '_' + 'lidar'
+                # obs_space_dict[name] = gymnasium.spaces.Box(
+                #     0.0,
+                #     1.0,
+                #     (self.lidar_conf.num_bins,),
+                #     dtype=np.float64,
+                # )
             if hasattr(obstacle, 'is_comp_observed') and obstacle.is_comp_observed:
                 name = obstacle.name + '_' + 'comp'
                 obs_space_dict[name] = gymnasium.spaces.Box(
@@ -258,7 +258,25 @@ class BaseTask(Underlying):  # pylint: disable=too-many-instance-attributes,too-
                 (*self.vision_env_conf.vision_size, 3),
                 dtype=np.uint8,
             )
+        obs_space_dict["joint_lidar"] = gymnasium.spaces.Box(
+                    0.0,
+                    1.0,
+                    (self.lidar_conf.num_bins,),
+                    dtype=np.float64,
+                )
+        obs_space_dict["goal_lidar"] = gymnasium.spaces.Box(
+                    0.0,
+                    1.0,
+                    (self.lidar_conf.num_bins,),
+                    dtype=np.float64,
+                )
 
+        obs_space_dict["joint_lidar_class"] = gymnasium.spaces.Box(
+                0,
+                8,
+                (self.lidar_conf.num_bins, ),
+                dtype=np.uint8,
+            )
         self.obs_info.obs_space_dict = gymnasium.spaces.Dict(obs_space_dict)
 
         if self.observation_flatten:
@@ -399,33 +417,23 @@ class BaseTask(Underlying):  # pylint: disable=too-many-instance-attributes,too-
 
         for obstacle in self._obstacles:
             if obstacle.is_lidar_observed:
-                obs[obstacle.name + '_lidar'] = self._obs_lidar(obstacle.pos, obstacle.group)
-                lidar = np.expand_dims(obs[obstacle.name + '_lidar'], 1)
-                lidar[lidar==0.] = 10.
+                lidar = self._obs_lidar(obstacle.pos, obstacle.group)
+                if obstacle.name == "goal":
+                    obs["goal_lidar"] = lidar
+                lidar = np.expand_dims(lidar, 1)
                 all_lidar = lidar if all_lidar is None else np.hstack((all_lidar, lidar))
-                lidar_class.append([obstacles.index(obstacle.name)]*obs[obstacle.name + '_lidar'].shape[0])
-            
+                lidar_class.append([obstacles.index(obstacle.name)]*lidar.shape[0])
+                # del obs[obstacle.name+'_lidar']
             if hasattr(obstacle, 'is_comp_observed') and obstacle.is_comp_observed:
                 obs[obstacle.name + '_comp'] = self._obs_compass(obstacle.pos)
-        print(all_lidar)
+
         all_lidar, lidar_class = np.array(all_lidar), np.transpose(np.array(lidar_class), (1,0))
-        print(all_lidar.shape, lidar_class.shape)
-        min_lidar_idx = np.argmin(all_lidar, axis=1)
-        #min_lidar_idx = np.hstack((np.expand_dims(range(len(min_lidar_idx)), 1), np.expand_dims(min_lidar_idx, 1)))
-        print(min_lidar_idx)
+        min_lidar_idx = np.argmax(all_lidar, axis=1)
         min_lidar_idx += np.array(list(range(0, np.array(all_lidar.shape).prod(), all_lidar.shape[1])))
-        print(min_lidar_idx)
-        #all_lidar, lidar_class = np.transpose(all_lidar, (1,0)), np.transpose(lidar_class, (1,0))
-#        lidar_bool = np.zeros_like(all_lidar).astype(np.bool)
-#        lidar_bool[min_lidar_idx] = True
-        print("Lidar index", min_lidar_idx)
-        # print(all_lidar.shape, lidar_class.shape, min_lidar_idx.shape)
-        print(np.take(all_lidar, min_lidar_idx))
 
         obs["joint_lidar"] = np.take(all_lidar, min_lidar_idx)
         obs["joint_lidar_class"] = np.take(lidar_class, min_lidar_idx)
 
-        print("Joint lidar is ", obs["joint_lidar"], obs["joint_lidar_class"])
         if self.observe_vision:
             obs['vision'] = self._obs_vision()
 
