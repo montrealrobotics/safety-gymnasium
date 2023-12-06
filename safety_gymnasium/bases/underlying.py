695adf8184b4c1dc8581d9d9ba97f5681797289b
Kaustubh Mani <kaustubh3095@gmail.com>
2023-12-06 13:50:36 -0500

    bug fix

--- safety_gymnasium/bases/underlying.py
+++ safety_gymnasium/bases/underlying.py
@@ -219,7 +219,7 @@ class Underlying(abc.ABC):  # pylint: disable=too-many-instance-attributes
         self.agent_name = None
         self.observe_vision = False  # Observe vision from the agent
         self.debug = False
-        self.observation_flatten = False  # Flatten observation into a vector
+        self.observation_flatten = True  # Flatten observation into a vector
         self._parse(config)
         self.agent = None
         self.action_noise: float = (
